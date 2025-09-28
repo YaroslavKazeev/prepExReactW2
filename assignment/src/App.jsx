62de747feeb94ebe93fb00032a7aa05f03687345
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-09-28 11:28:24 +0200

    exercise is done

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -4,23 +4,24 @@ import "./App.css";
 function App() {
   const [person, setPerson] = useState(null);
 
-  // Use useEffect to call getPerson when component mounts
-  useEffect(() => {
-    async function getPerson() {
-      const response = await fetch("https://randomuser.me/api?results=1");
-      const data = await response.json();
-      setPerson(data.results[0]);
-    }
+  async function getPerson() {
+    const response = await fetch("https://randomuser.me/api?results=1");
+    const data = await response.json();
+    setPerson(data.results[0]);
+  }
 
-    getPerson();
-  }, []); // Empty dependency array means this runs once on mount
+  useEffect(() => getPerson, []);
   console.log(person);
 
   return (
     <>
-      <p>
-        {person ? `${person.name.first} ${person.name.last}` : "Loading..."}
-      </p>
+      <ul>
+        <li>
+          {person
+            ? `${person.name.first} ${person.name.last} ${person.email}`
+            : "Loading..."}
+        </li>
+      </ul>
     </>
   );
 }
