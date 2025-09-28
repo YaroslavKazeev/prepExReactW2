cc23202f587115c3e45d177b2a0dcb80ff4fdd8e
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-09-28 10:30:20 +0200

    Steps 2-4 done

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [person, setPerson] = useState(null);
 
+  // Use useEffect to call getPerson when component mounts
+  useEffect(() => {
+    async function getPerson() {
+      const response = await fetch("https://randomuser.me/api?results=1");
+      const data = await response.json();
+      setPerson(data.results[0]);
+    }
+
+    getPerson();
+  }, []); // Empty dependency array means this runs once on mount
+  console.log(person);
+
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {person ? `${person.name.first} ${person.name.last}` : "Loading..."}
       </p>
     </>
   );
